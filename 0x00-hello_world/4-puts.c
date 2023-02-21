d0685e68d568a69d897d9fe6ce5033b576cd5c60
Yasmeen <yasmeenkhalil229@gmail.com>
2023-02-20 17:23:25 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main-prints "Programming is like building a multilingual puzzle"
+ * Description:using main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * followed by a new line
+ * using the function puts
+ * Return:0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
