a599f2958429f2bbfdbbb94fbba6aa3a66ee29ed
Yasmeen <yasmeenkhalil229@gmail.com>
2023-02-27 18:01:26 -0800

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+ size_t strlen(const char *s);
+ void puts_half(char *str);
+
+ #endif /* MAIN_H */
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "7-main.h"
+#include <stdio.h>
+
+/**
+  * puts_half -  prints a string
+  *
+  * @str: arg *str
+  *
+  * Return: string to stdout
+  */
+
+void puts_half(char *str)
+{
+	int half_len;
+
+	half_len = strlen(str) / 2;
+	while (str[half_len] != 0)
+	{
+		putchar(str[half_len]);
+		(half_len)++;
+	}
+}
