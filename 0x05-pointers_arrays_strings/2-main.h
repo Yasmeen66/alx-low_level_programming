b8fe0221ed42374f14c0631d2e5559c1b0327ac9
Yasmeen <yasmeenkhalil229@gmail.com>
2023-02-27 16:13:00 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _strlen(char *s);
+size_t strlen(const char *s);
+
+#endif /* MAIN_H */
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "2-main.h"
+#include <stdio.h>
+
+/**
+ * _strlen -  returns the length of a string.
+ * @s: sequence of char
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
