78a06981f9f61c276c5a53c178a1c285fad43cc6
Yasmeen <yasmeenkhalil229@gmail.com>
2023-03-02 14:53:24 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int strcmp(const char *s1, const char *s2);
+int _strcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "3-main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: string variable
+ * @s2: string variable
+ * Return: int number
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+
+	return (res);
+}
