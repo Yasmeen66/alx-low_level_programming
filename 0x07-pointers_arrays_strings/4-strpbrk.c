35e6b89ad9c9a02d54907ce33cf4817393f74693
Yasmeen <yasmeenkhalil229@gmail.com>
2023-03-06 18:31:56 -0800

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,31 +10,9 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j, pos, flag = 0;
+	char *ptr;
 
-	/*for (i = 0; s[i] != '\0'; i++);*/
-	pos = i;
+	ptr = strpbrk(s, accept);
 
-	for (i = 0; accept[i] != '\0'; i++)
-	{
-		for (j = 0; s[j] != '\0'; j++)
-		{
-			if (accept[i] == s[j])
-			{
-				if (j <= pos)
-				{
-					pos = j;
-					flag = 1;
-				}
-			}
-		}
-	}
-	if (flag == 1)
-	{
-		return (&s[pos]);
-	}
-	else
-	{
-		return (NULL);
-	}
+	return (ptr);
 }
